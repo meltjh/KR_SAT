4d84bf1aa9df4d9cc00190df48ea0d7edd86c0c6
Melissa Tjhia <melissatjhia@wcw-stud-145-109-53-252.wireless.uva.nl>
2017-09-29 16:58:14 +0200

    glucose sat solver

---
+++ glucose-syrup-4.1/simp/depend.mk
@@ -0,0 +1,42 @@
+/Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/Main.o /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/Main.or /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/Main.od /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/Main.op: \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/Main.cc \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../utils/System.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../utils/Options.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Vec.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/Dimacs.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Alg.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Map.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Queue.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/Solver.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Heap.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/Constants.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Clone.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/SolverStats.h
+/Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/SimpSolver.o /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/SimpSolver.or /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/SimpSolver.od /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/SimpSolver.op: \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/SimpSolver.cc \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Sort.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Vec.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Queue.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/Solver.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Heap.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Alg.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../utils/Options.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Map.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/Constants.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../mtl/Clone.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../core/SolverStats.h \
+  /Users/melissatjhia/Documents/jaar1ai/Knowledge_Representation/Sudoku_project/KR_SAT/glucose-syrup-4.1/simp/../utils/System.h
